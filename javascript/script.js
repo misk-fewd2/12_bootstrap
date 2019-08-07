a1b767e46a4ba24b90da05a3ac56d8f4453f0948
Marc Wright <marc.wright@gmail.com>
2019-08-07 20:41:02 +0300

    In class code

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,12 @@
 console.log("loaded")
+
+$('#exampleModal2').on('click', () => {
+  console.log("model clicked!");
+
+  setTimeout(() => {
+    $('#spinner-div')
+      .empty()
+      .html('<h1>Hello Misk!</h1>')
+  }, 1000)
+
+})
